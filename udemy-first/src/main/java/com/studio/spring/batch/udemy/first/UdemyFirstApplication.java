339284b2caa2a12b26f4a0b193261015cfec6dac
valdir junior <valdir.junior.dev@gmail.com>
2024-03-15 19:37:25 -0300

    configurando mysql

---
+++ udemy-first/src/main/java/com/studio/spring/batch/udemy/first/UdemyFirstApplication.java
@@ -0,0 +1,13 @@
+package com.studio.spring.batch.udemy.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyFirstApplication.class, args);
+	}
+
+}
---
+++ udemy-first/src/test/java/com/studio/spring/batch/udemy/first/udemyfirst/UdemyFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.spring.batch.udemy.first.udemyfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
